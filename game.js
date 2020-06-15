81f89529bef5496ba931f3ec31c5e3fa697858d2
Santiago Villa <santiagovillavega@gmail.com>
2020-06-15 19:14:24 -0300

    Canvas + Speed 1, 2 y 3

---
+++ game.js
@@ -0,0 +1,139 @@
+(function(){
+    'use strict';
+    window.addEventListener('load', init, false);
+    var KEY_LEFT = 37;
+    var KEY_RIGHT = 39;
+    var K1 = 10;
+    var K2 = 20;
+    var K3 = 0.5;
+
+    var canvas = null, ctx = null;
+    var lastPress = null;
+    var pressing = [];
+    var player1 = new Circle(40, 40, 5);
+    var player2 = new Circle(40, 100, 5);
+    var player3 = new Circle(40, 160, 5);
+    var speed1 = 0;
+    var speed2 = 0;
+    var speed3 = 0;
+
+    function init() {
+        canvas = document.getElementById('canvas');
+        ctx = canvas.getContext('2d');
+        canvas.width = 300;
+        canvas.height = 200;
+
+        run();
+        repaint();
+    }
+
+    function run() {
+        setTimeout(run, 50);
+        act(0.05);
+    }
+
+    function repaint() {
+        requestAnimationFrame(repaint);
+        paint(ctx);
+    }
+
+    function act(deltaTime) {
+        //Move Rect
+        speed1 = 0;
+        if (pressing[KEY_RIGHT]) {
+            speed1 += K1;
+            speed2 += K2;
+            if (speed3 < 10)
+                speed3 += K3;
+        }
+        else {
+            if (speed3 > 0)
+                speed3 -= K3;
+        }
+        if (pressing[KEY_LEFT]) {
+            speed1 -= K1;
+            speed2 -= K2;
+            if (speed3 > -10)
+                speed3 -= K3;
+        }
+        else {
+            if (speed3 < 0)
+                speed3 += K3;
+        }
+        speed2 *= 0.9;
+
+        player1.x += speed1;
+        player2.x += speed2;
+        player3.x += speed3;
+
+        //Out Screen
+        if (player1.x > canvas.width)
+            player1.x = 0;
+        if (player1.x < 0)
+            player1.x = canvas.width;
+            
+        if (player2.x > canvas.width)
+            player2.x = 0;
+        if (player2.x < 0)
+            player2.x = canvas.width;
+            
+        if (player3.x > canvas.width)
+            player3.x = 0;
+        if (player3.x < 0)
+            player3.x = canvas.width;    
+    }
+
+    function paint(ctx) {
+        ctx.fillStyle = '#000';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        ctx.strokeStyle = '#0f0';
+        player1.stroke(ctx);
+        player2.stroke(ctx);
+        player3.stroke(ctx);
+
+        ctx.fillStyle = '#fff';
+        //ctx.fillText('Last Press: '+lastPress, 0, 20);
+        ctx.fillText('Speed1: '+speed1, 0, 10);
+        ctx.fillText('Speed2: '+speed2, 100, 10);
+        ctx.fillText('Speed3: '+speed3, 0, 20);
+    }
+
+    document.addEventListener('keydown', function(evt){
+        lastPress = evt.keyCode;
+        pressing[evt.keyCode] = true;
+    }, false);
+
+    document.addEventListener('keyup', function(evt){
+        pressing[evt.keyCode] = false;
+    }, false);
+
+    function Circle(x, y, radius) {
+        this.x = (x == null) ? 0 : x;
+        this.y = (y == null) ? 0 : y;
+        this.radius = (radius == null) ? 0 : radius;
+    }
+
+    Circle.prototype.distance = function(circle) {
+        if (circle != null) {
+            var dx = this.x - circle.x;
+            var dy = this.y - circle.y;
+            return (Math.sqrt(dx*dx + dy*dy) - (this.radius + circle.radius));
+        }
+    }
+
+    Circle.prototype.stroke = function(ctx) {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, true);
+        ctx.stroke();
+    }
+
+    window.requestAnimationFrame = (function() {
+        return window.requestAnimationFrame ||
+               window.webkitRequestAnimationFrame ||
+               window.mozRequestAnimationFrame ||
+               function(callback) {
+                   window.setTimeout(callback, 17);
+               };  
+    })();
+})();
